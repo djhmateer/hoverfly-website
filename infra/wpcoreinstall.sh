96976994acb5c9cead94fd0d181f6e01eae9937f
Dave Mateer <davemateer@gmail.com>
2020-10-03 15:47:52 +0100

    auto

---
+++ infra/wpcoreinstall.sh
@@ -0,0 +1,1 @@
+sudo -u www-data wp core install --url='http://hoverflytest165.westeurope.cloudapp.azure.com' --title='Blog Title' --admin_user='dave' --admin_password='letmein' --admin_email='email@domain.com'
