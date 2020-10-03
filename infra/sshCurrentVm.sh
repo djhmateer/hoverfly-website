099d592036b1edc70e41643b6e73224b15f3caf9
Dave Mateer <davemateer@gmail.com>
2020-10-03 17:05:11 +0100

    auto

--- infra/sshCurrentVm.sh
+++ infra/sshCurrentVm.sh
@@ -1,3 +1,3 @@
 
-ssh -o StrictHostKeyChecking=no dave@hoverflytest427.westeurope.cloudapp.azure.com
+ssh -o StrictHostKeyChecking=no dave@hoverflytest265.westeurope.cloudapp.azure.com
 
--- infra/wpcoreinstall.sh
+++ infra/wpcoreinstall.sh
@@ -1,1 +1,1 @@
-sudo -u www-data wp core install --url='http://hoverflytest265.westeurope.cloudapp.azure.com' --title='Blog Title' --admin_user='dave' --admin_password='letmein' --admin_email='email@domain.com'
+wp core install --url='http://hoverflytest735.westeurope.cloudapp.azure.com' --title='Blog Title' --admin_user='dave' --admin_password='letmein' --admin_email='email@domain.com'
