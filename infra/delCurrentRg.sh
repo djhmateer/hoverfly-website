d87e56a78a045c65ac2dbab5dfc994851780901a
Dave Mateer <davemateer@gmail.com>
2021-11-25 07:18:12 +0000

    auto

--- infra/delCurrentRg.sh
+++ infra/delCurrentRg.sh
@@ -1,2 +1,2 @@
-az group delete --name HoverflyTEST704 --no-wait --yes 
+az group delete --name HoverflyLagoons51 --no-wait --yes 
 
--- infra/sshCurrentVm.sh
+++ infra/sshCurrentVm.sh
@@ -1,3 +1,3 @@
 
-ssh -o StrictHostKeyChecking=no dave@hoverflytest704.westeurope.cloudapp.azure.com
+ssh -o StrictHostKeyChecking=no dave@hoverflylagoons51.westeurope.cloudapp.azure.com
 
